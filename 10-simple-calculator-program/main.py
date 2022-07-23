d0fbc146a894aca2839f605f67bd4b2e1cc9a340
NjohPrince <jufredprince@gmail.com>
2022-07-23 10:17:15 +0100

    simple calculator in python

--- 10-simple-calculator-program/main.py
+++ 10-simple-calculator-program/main.py
@@ -21,10 +21,32 @@ print("\n{} welcome to simplo-calculator.\n".format(username))
 print("Operation Set:")
 print("1. Addition \n2. Subtraction \n3. Multiplication \n4. Division")
 
-operation = input("Select an operation: ")
-num_one = input("\nEnter a number: ")
-num_two = input("Enter another number")
+# creating a dictionary to track chosen operator
+match_operators_to_selection = {1: "+", 2: "-", 3: "*", 4: "/"}
 
-print(f"\nExpression is {num_one + ' ' + operation + ' ' + num_two}\n")
+# converted input into an integer
+operation = int(input("\nSelect an operation: "))
 
-print(f"\nResult is \n")
+# check if operator is within our available options
+if operation not in range(1, 5):
+    print("Invalid operator!")
+    exit(0)  # exit the program
+
+num_one = int(input("\nEnter a number: "))  # converted input into an integer
+# converted input into an integer
+num_two = int(input("Enter another number: "))
+
+print(
+    f"\nExpression is {str(num_one) + ' ' + match_operators_to_selection[operation] + ' ' + str(num_two)}")
+
+if operation == 1:
+    print("\nResult is ", num_one + num_two)
+elif operation == 2:
+    print("\nResult is ", num_one - num_two)
+elif operation == 3:
+    print("\nResult is ", num_one * num_two)
+elif operation == 4:
+    if num_two == 0:  # division by zero error
+        print("\nError division by zero is not allowed.")
+    else:
+        print("\nResult is ", num_one / num_two)
