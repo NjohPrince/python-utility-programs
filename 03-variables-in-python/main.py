572480a29237bda96d9f448210ad98bd0fc68fca
NjohPrince <jufredprince@gmail.com>
2022-07-23 05:19:51 +0100

    type of variables in python

--- 03-variables-in-python/main.py
+++ 03-variables-in-python/main.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python
+
+"""main.py: comments in python"""
+
+__author__ = "Njoh Noh Prince Junior"
+__copyright__ = "Copyright 2022"
+__credits__ = []
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Njoh Noh Prince Junior"
+__email__ = "jufredprince@gmail.com"
+
+# program starts here - igonore what is above
+
 # A Python variable is a symbolic name that is a reference or
 # pointer to an object. Variables can later on be refered to
 # within the program by their names and can be assinged
---
+++ 04-variable-types-in-python/main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""main.py: comments in python"""
+
+__author__ = "Njoh Noh Prince Junior"
+__copyright__ = "Copyright 2022"
+__credits__ = []
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Njoh Noh Prince Junior"
+__email__ = "jufredprince@gmail.com"
+
+# program starts here - igonore what is above
+
+# A Python variable is a symbolic name that is a reference or
+# pointer to an object. Variables can later on be refered to
+# within the program by their names and can be assinged
+# values.
+
+# variables declaration
+
+first_name = "Njoh Noh"
+last_name = "Prince Junior"
+age = 20
+height = 1.75
+is_admin = True
+dictionary_blog = {'blog_title': 'Python Guru'}
+list_of_numbers = [1, 2, 3]
+tuple_numbers = (19, 21, 46)
+
+# know variable datatypes using type keyword
+
+print(type(first_name)) # <class 'str'>
+print(type(last_name)) # <class 'str'>
+print(type(age)) # <class 'int'>
+print(type(height)) # <class 'float'>
+print(type(is_admin)) # <class 'bool'>
+print(type(dictionary_blog)) # <class 'dict'>
+print(type(list_of_numbers)) # <class 'list'>
+print(type(tuple_numbers)) # <class 'tuple'>
