fd9d13f8872404994ef2376b8d320e606c8a5ca3
NjohPrince <jufredprince@gmail.com>
2022-07-23 05:12:16 +0100

    variables in python

---
+++ variables-in-python/main.py
@@ -0,0 +1,31 @@
+# A Python variable is a symbolic name that is a reference or
+# pointer to an object. Variables can later on be refered to
+# within the program by their names and can be assinged
+# values.
+
+first_name = "Njoh Noh"  # variable first_name
+last_name = "Prince Junior"  # variable last_name
+
+print(first_name)  # prints the data container within the variable first_name
+print(last_name)  # prints the data container within the variable last_name
+
+# Examples of Variable Naming conventions
+
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, firstName and FirstName are three different variables)
+
+# Good naming conventions
+
+FirstName = "Njoh Noh"  # pascal casing -can be confusing when using classes
+firstName = "Njoh Noh"  # camel casing
+first_name = "Njoh Noh"  # snake casing -preferred
+_firstName = "Njoh Noh"
+
+# Bad or illegal naming conventions
+
+# 👇 delete the leading hashes then run and observe the error
+# 2firstName = "Njoh Noh"
+# first-name = "Njoh Noh"
+# first name = "Njoh Noh"
