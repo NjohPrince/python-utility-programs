8f121f719106161a894b99d2564122e05616e227
NjohPrince <jufredprince@gmail.com>
2022-07-23 12:24:32 +0100

    structuring and working woth large python projects

---
+++ 17-structuring-large-projects/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""main.py: structuring and working with large projects in python"""
+
+__author__ = "Njoh Noh Prince Junior"
+__copyright__ = "Copyright 2022"
+__credits__ = []
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Njoh Noh Prince Junior"
+__email__ = "jufredprince@gmail.com"
+
+# program starts here - igonore what is above
