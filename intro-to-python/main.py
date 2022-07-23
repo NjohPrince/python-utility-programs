aeab26937481066d5294276246b3e993925e09b6
NjohPrince <jufredprince@gmail.com>
2022-07-23 04:42:30 +0100

    introduction to python

---
+++ intro-to-python/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""main.py: Prints the message Hello World. Feels good to learn python!"""
+
+__author__ = "Njoh Noh Prince Junior"
+__copyright__ = "Copyright 2022"
+__credits__ = []
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Njoh Noh Prince Junior"
+__email__ = "jufredprince@gmail.com"
+
+# program starts here - igonore what is above
+
+print("Hello World. Feels good to learn python!")
