995817a79e20990d3086cd26ef478b16a48db9c4
NjohPrince <jufredprince@gmail.com>
2022-07-23 06:21:48 +0100

    datatype conversion

--- 06-datatype-conversion/main.py
+++ 06-datatype-conversion/main.py
@@ -11,3 +11,25 @@ __maintainer__ = "Njoh Noh Prince Junior"
 __email__ = "jufredprince@gmail.com"
 
 # program starts here - igonore what is above
+
+from ast import Num
+
+
+data = "123"
+
+print(type(data))  # <class 'str'>
+
+print(int(data))  # 123
+print(type(int(data)))  # <class 'int'>
+
+print(float(data))  # 123.0
+
+character = 'c'
+
+# converts and displays the ascii representation of the string
+print(ord(character))
+
+python_string = 'python'
+converted_to_tuple = tuple(python_string)
+# ('p', 'y', 't', 'h', 'o', 'n')
+print("string converted into tuple:  ", converted_to_tuple)
