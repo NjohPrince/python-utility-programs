909dc98a3152230e814020103b9c3faf04dd995a
NjohPrince <jufredprince@gmail.com>
2022-07-23 11:58:08 +0100

    inheritance

--- 15-OOP-concepts/inheritance.py
+++ 15-OOP-concepts/inheritance.py
@@ -4,3 +4,19 @@
 # properties from another class. Parent class is the class being inherited
 # from, also called base class. Child class is the class that inherits
 # from another class, also called derived class.
+
+class Car():
+    def category(self):
+        print("Ferrari")
+
+    def color(self):
+        print("Color: Red")
+
+
+class BadCars(Car):
+    pass  # no properties or methods
+
+
+car_obj = BadCars()
+
+car_obj.category()  # Ferrari
