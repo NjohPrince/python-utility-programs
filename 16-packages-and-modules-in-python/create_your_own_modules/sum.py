5af4d8a137e0df8829b283b392ed55b33d26b57a
NjohPrince <jufredprince@gmail.com>
2022-07-23 23:43:02 +0100

    sum.py fix

--- 16-packages-and-modules-in-python/create_your_own_modules/sum.py
+++ 16-packages-and-modules-in-python/create_your_own_modules/sum.py
@@ -1,11 +1,4 @@
 def sum(num_one, num_two):
     """Sum two given numbers
-
-    Args:
-        num_one (_int_): _variable to hold first number_
-        num_two (_int_): _variable to hold second number_
-
-    Returns:
-        _type_: _description_
     """
     return num_one + num_two
