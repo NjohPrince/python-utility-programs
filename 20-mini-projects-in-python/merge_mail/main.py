4d4b30a8ebcab43dc5d1faed770f03d9d3e9714f
Suprememajor <ndipnobertetta@gmail.com>
2022-07-30 23:31:11 +0100

    Refactored based on knowledge on comprehensions

--- 20-mini-projects-in-python/merge_mail/main.py
+++ 20-mini-projects-in-python/merge_mail/main.py
@@ -1,9 +1,7 @@
 PLACEHOLDER = "[name]"
 
 with open("input/invited_names.txt") as names_file:
-    names_list = []
-    for line in names_file:
-        names_list.append(line.strip())
+    names_list = [line.strip() for line in names_file]
 
 with open("input/starting_letter.docx") as file:
     text = file.read()
